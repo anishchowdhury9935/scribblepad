2a954fd1c95238e6cff64da80c096a8f7d71a5ff
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-01-06 02:40:48 +0530

    update

--- inotebook_backend/index.js
+++ inotebook_backend/index.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000
+const port = 5000
 const connectToMongoose = require('./db.js')
 app.use(cors());
 

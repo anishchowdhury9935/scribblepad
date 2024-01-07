1a3178949d7d8013331de3ff95262dc664aabfe4
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-01-07 20:40:56 +0530

    upad

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div>
         <Router>
       <NoteState>
-        <Navbar companyName='ScribblePad'/>
+        <Navbar companyName='Scribble pad'/>
         <Alert/>
           <div className="middle-content container" style={{"padding":"0 10vw","overflow":"hidden"}}>
             {/* all Routes */}

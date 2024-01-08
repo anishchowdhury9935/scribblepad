29a241535589c0597bcf7f0116276a5f00812580
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-01-09 02:23:58 +0530

    fix : update App.css bug Fixed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from "react"
 import noteContext from '../context/noteContext';
 export default function Alert() {
     const {Alert,AlertStyle,allAlert,AlertType} = useContext(noteContext)
-    setTimeout(() => {
-        allAlert("","",{"display": "none"})
-    }, 5000);
+    // setTimeout(() => {
+    //     allAlert("","",{"display": "none"})
+    // }, 5000);
     return (
         <div style={{...AlertStyle,"position":"absolute","marginTop":"0.25em","marginLeft":"0.25em","width":"99.5vw"}} id="alert-qp30">
             <div style={{"width":"auto","display":"flex","justifyContent":"flex-end"}}>

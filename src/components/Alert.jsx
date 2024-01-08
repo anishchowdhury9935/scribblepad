e2f6da2030bb0a012615c866427d608a573607f5
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-01-09 02:08:20 +0530

    style : updated App.css for phone users

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -6,7 +6,7 @@ export default function Alert() {
         allAlert("","",{"display": "none"})
     }, 5000);
     return (
-        <div style={{...AlertStyle,"position":"absolute","marginTop":"4px","marginLeft":"4px","width":"99.5vw"}}>
+        <div style={{...AlertStyle,"position":"absolute","marginTop":"0.25em","marginLeft":"0.25em","width":"99.5vw"}} id="alert-qp30">
             <div style={{"width":"auto","display":"flex","justifyContent":"flex-end"}}>
                 <div className={`alert alert-${AlertType}`} role="alert">
                     {Alert}

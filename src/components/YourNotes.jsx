63a4c98b1f22099684ab6ddcf687ad2b0e856407
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-02-29 02:05:12 +0530

    Update : YourNotes.jsx

--- src/components/YourNotes.jsx
+++ src/components/YourNotes.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 export default function YourNotes() {
     const navigate = useNavigate()
     const context = useContext(noteContext)
-    const { allAlert,getNotes } = context;
+    const { allAlert,getNotes,setProgress } = context;
     function first_signup() {
         if (localStorage.getItem('token') === "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7fSwiaWF0IjoxNzA0NDY5NDk3fQ.uZX3xZzBt5GJY0ZhhtDQ9kPkTRURtvwcKq8wAYcnKPM") {
             allAlert("please login or sign up first",'danger')
@@ -15,6 +15,7 @@ export default function YourNotes() {
     useEffect(()=>{
         first_signup()
         getNotes()
+        return setProgress(0)
     },[])
     return (
         <>

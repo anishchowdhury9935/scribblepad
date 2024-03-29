28c4cc4d7ff6973af981e3da45c7e26536425e25
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-03-30 00:44:57 +0530

    Update : Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -4,7 +4,7 @@ export default function Alert() {
     const {Alert,AlertStyle,allAlert,AlertType} = useContext(noteContext)
         setTimeout(() => {
             allAlert("","",{"display": "none"})
-        }, 6500)
+        }, 12000)
     return (
         <div style={{...AlertStyle,"position":"absolute","marginTop":"0.25em","marginLeft":"0.25em","width":"99.5vw"}} id="alert-qp30">
             <div style={{"width":"auto","display":"flex","justifyContent":"flex-end"}}>
